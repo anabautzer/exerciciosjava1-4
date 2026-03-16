5b3e5fa7ebdf954c421dee9fd8f76ced96c930bf
anabautzer <ac.bautzer@hotmail.com>
2026-03-16 18:46:40 -0300

    Add files via upload

---
+++ exercicio-conversor/ConversorDeMedidas.java
@@ -0,0 +1,14 @@
+public class ConversorDeMedidas {
+    double centimetrosParaPolegadas (double centimetros) {
+        return centimetros / 2.54;
+    }
+    double polegadasParaCentimetros (double polegadas) {
+        return  polegadas * 2.54;
+    }
+    double kmParaMetros (double quilometros) {
+        return quilometros * 1000.0;
+    }
+    double metrosParaKm (double metros) {
+        return metros / 1000.0;
+    }
+}
---
+++ exercicio-conversor/TesteConversorDeMedidas.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TesteConversorDeMedidas {
+    public static void main(String[] args) {
+        Scanner sa = new Scanner(System.in);
+        ConversorDeMedidas cm = new ConversorDeMedidas();
+        System.out.println("Escreva um valor em centímetros: ");
+        double centimetros = sa.nextDouble();
+        System.out.println(centimetros + " em polegadas é " + cm.centimetrosParaPolegadas(centimetros));
+        System.out.println("Escreva um valor em polegadas: ");
+        double polegadas = sa.nextDouble();
+        System.out.println(polegadas + " em centímetros é " + cm.polegadasParaCentimetros(polegadas));
+        System.out.println("Escreva um valor em quilómetros: ");
+        double quilometros = sa.nextDouble();
+        System.out.println(quilometros + " em metros é " + cm.kmParaMetros(quilometros));
+        System.out.println("Escreva um valor em metros: ");
+        double metros = sa.nextDouble();
+        System.out.println(metros + " em quilómetros é " + cm.metrosParaKm(metros));
+    }
+}
