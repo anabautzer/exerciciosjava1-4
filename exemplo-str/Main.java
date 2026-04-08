bd70ec9a324cf088410cea6c13fada106cab58e2
Ana Carolina Bautzer <ac.bautzer@hotmail.com>
2026-04-08 12:08:49 -0300

    Add files via upload

---
+++ exemplo-str/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    String nome = "Ana Carolina";
+    String substring = nome.substring(5,10);
+        System.out.println(nome);
+        System.out.println(substring);
+    String trim = nome.trim();
+        System.out.println(trim);
+    char letra = nome.charAt(7);
+        System.out.println(letra);
+    }
+}
