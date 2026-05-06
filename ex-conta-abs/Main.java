9ff5e54f68ccbb442c3e7ab392c5469889449c32
Ana Carolina Bautzer <ac.bautzer@hotmail.com>
2026-05-06 12:02:54 -0300

    Update Main.java

--- ex-conta-abs/Main.java
+++ ex-conta-abs/Main.java
@@ -4,42 +4,35 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ContaCorrente cc = new ContaCorrente(1, 500);
-        ContaPoupanca cp = new ContaPoupanca(1, 500);
-        System.out.println("Em qual conta você quer mexer?");
-        System.out.println("Conta Corrente (cc) ou Conta Poupança (cp)?");
-        String conta = sc.nextLine();
-        System.out.println("O que você deseja fazer?");
-        System.out.println("Digite 1 para depositar");
-        System.out.println("Digite 2 para sacar");
-        System.out.println("Digite 3 para sair");
-        int opcao = sc.nextInt();
-        while (true) {
-            if (conta.equals("cc")) {
-                if (opcao == 1) {
-                    cc.depositar(sc.nextDouble());
-                    break;
-                } else if (opcao == 2) {
-                    cc.sacar(sc.nextDouble());
-                    break;
-                } else {
-                    System.out.println("Finalizando");
-                    break;
-                }
-            }else if (conta.equals("cp")) {
-                if (opcao == 1) {
-                    cp.depositar(sc.nextDouble());
-                    break;
-                } else if (opcao == 2){
-                    cp.sacar(sc.nextDouble());
-                    break;
-                } else{
-                    System.out.println("Finalizando");
-                    break;
-                    }
-            }
 
+        Scanner sc = new Scanner(System.in);
+        Conta c;
+        System.out.println("Digite qual é a conta?");
+        while (true){
+            int opcao = Integer.parseInt(sc.nextLine());
+            if (opcao == 1){
+                c = new ContaCorrente(5, 5000);
+                break;
+            } else if (opcao == 2) {
+                c = new ContaPoupanca(4, 2000);
+                break;
+            }
+        }
+        while (true){
+            System.out.println("Digite sua opção entre 1 e 3");
+            int opcao = Integer.parseInt(sc.nextLine());
+            if (opcao == 1) {
+                c.depositar(500);
+                System.out.println("Depositando: " + c.saldo);
+                break;
+            } else if (opcao == 2) {
+                c.sacar(500);
+                System.out.println("Sacando: " + c.saldo);
+                break;
+            } else {
+                System.out.println("Finalizando");
+                break;
+            }
         }
     }
 }
